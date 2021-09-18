d055a0abdb111f18d2d60d454aec620f806460f8
yuuki-matsunaga <matsunaga.ymk@gmail.com>
2021-09-18 15:25:00 +0000

    Add simple_formatの追加

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,14 +3,12 @@ class GoodsController < ApplicationController
   def create
     @post = Post.find(params[:post][:post_id])
     current_user.good(@post)
-    # binding.pry
   end
 
   def destroy
-    # binding.pry
     @post = Post.find(params[:id])
     current_user.ungood(@post)
- 
+
   end
 
 end
