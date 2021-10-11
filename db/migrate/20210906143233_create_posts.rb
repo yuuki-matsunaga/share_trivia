a998569db03ea9ee36549f4b96152f44c5ef5f5e
yuuki-matsunaga <matsunaga.ymk@gmail.com>
2021-10-11 10:45:31 +0000

    Fix 不具合修正

--- db/migrate/20210906143233_create_posts.rb
+++ db/migrate/20210906143233_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.integer :user_id, null: false, default: ""
-      t.integer :genre_id, null: false, default: ""
+      t.integer :user_id, null: false
+      t.integer :genre_id, null: false
       t.string :title, null: false, default: ""
       t.string :image_id, null: false, default: ""
       t.text :introduction, null: false, default: ""
